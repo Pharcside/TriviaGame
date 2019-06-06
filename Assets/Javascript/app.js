18e600c48eba0053d939812ca8b61454fdf382c5
Brian Corbett <briancorbett@brighthouse.com>
2019-06-06 17:19:58 -0400

    added stylesheets to html. / added pseudo code to app.js

---
+++ Assets/Javascript/app.js
@@ -0,0 +1,9 @@
+
+
+var questions = [
+    {
+      question: "What color was Pacman?",
+      choices: ["red","blue","yellow","pink","black"],
+      correctAnswer: "yellow",
+      image: "<img src='Assets/images/Pacman.jpg>"
+    },
